210b72b63494c8e5952d1a87bcfa74b43b90a9ee
Jared Leander <jleander12@gmail.com>
2023-05-07 19:03:51 -0700

    Complete exercise 02 on loops and functions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeats) {
+    let stringSum = '';
+    if (numRepeats < 0) {
+        return 'ERROR';
+    }
+    for (numRepeats; numRepeats - 1 >= 0; numRepeats--) {
+        stringSum += string;
+    }
+    return stringSum;
 };
 
 // Do not edit below this line
