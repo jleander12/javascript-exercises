ac8fc33eaf7517b82ab8201c4faa458f6443fdaa
Jared Leander <jleander12@gmail.com>
2023-05-07 21:36:35 -0700

    Write array manipulation function for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, itemRemove) {
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = array.length - 1; j >= 0; j--) {
+            if (array[j] === arguments[i]) {
+                array.splice(j,1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
