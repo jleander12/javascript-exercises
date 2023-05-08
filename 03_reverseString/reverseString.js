441ae0a3a9a85b04dc070776b159ad3bbe2ecaa2
Jared Leander <jleander12@gmail.com>
2023-05-07 21:03:01 -0700

    Complete exercise 3 looping string manipulation

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let inverseString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        inverseString += string.substr(i,1);
+    }
+    return inverseString;
 };
 
 // Do not edit below this line
