de82c2106592635ed8566a876d768df2b5ff40ec
Jared Leander <jleander12@gmail.com>
2023-05-07 21:55:57 -0700

    Solve exercise 05 - conditional loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(smallNum, bigNum) {
+    
+    //check that the arguments are numerical
+    if (smallNum < 0 || bigNum < 0 || typeof smallNum != 'number' || typeof bigNum != 'number') {
+        return `ERROR`
+    }
 
+    //sum all numbers between the 2 arguments
+    let sum = 0;
+    if (smallNum > bigNum) {
+        for (let i = bigNum; i <= smallNum; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = smallNum; i <= bigNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
