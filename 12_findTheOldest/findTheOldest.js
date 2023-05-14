dd158ca6e72aef6e594c378d9245eca8ff2f0f9b
Jared Leander <jleander12@gmail.com>
2023-05-13 22:58:32 -0700

    Complete exercise 12 with array sorting

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const age = people.sort((a, b) => {
+        let newAge = b.yearOfDeath - b.yearOfBirth;
+        if (Number.isNaN(newAge)) {
+            newAge = (new Date()).getFullYear() - b.yearOfBirth;
+        }
+        let previousAge = a.yearOfDeath - a.yearOfBirth;
+        if (Number.isNaN(previousAge)) {
+            previousAge = (new Date()).getFullYear() - a.yearOfBirth;
+        }
+        console.log(newAge);
+        if (newAge > previousAge) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+    return age[0];
 };
 
 // Do not edit below this line
