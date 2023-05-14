0de1293caf6ddf0f00df23e398b47b697ae14087
Jared Leander <jleander12@gmail.com>
2023-05-13 22:17:10 -0700

    Solve exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 1) return 'OOPS'; //reject 0 and negative
+    if (typeof index != 'number') {
+        index = Number(index);
+    }
+    let lastNum = 0;
+    let tempNum = 0;
+    let currentNum = 1;
+    for (let i = 0; i < index - 1; i++){
+        tempNum = currentNum;
+        currentNum += lastNum;
+        lastNum = tempNum;
+    }
+    return currentNum;
 };
 
 // Do not edit below this line
