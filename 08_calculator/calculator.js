8238d1d1d4b4762de6d419950206f2c16559d141
Jared Leander <jleander12@gmail.com>
2023-05-13 21:48:45 -0700

    Complete exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = 1;
+  for (let i = 0; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  while (a > 0) {
+    total *= a;
+    a--
+  }
+  return total;
 };
 
 // Do not edit below this line
